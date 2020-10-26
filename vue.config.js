57073aebc9569d5783f9aef5d73591635ef6b8e7
Nadir <chipchroumme@gmail.com>
2020-10-26 18:28:55 +0400

    splitting chuncks clear()

--- vue.config.js
+++ vue.config.js
@@ -7,47 +7,47 @@ module.exports = {
     plugins: [
       new webpack.ContextReplacementPlugin(/moment[\/\\]locale$/, /ru/)
     ],
-    optimization: {
-      splitChunks: {
-        cacheGroups: {
-          default: false,
-          // Custom common chunk
-          bundle: {
-            name: 'app',
-            chunks: 'all',
-            minChunks: 1,
-            reuseExistingChunk: true,
-            enforce: true,
-          },
-          // Customer vendor
-          vendors: {
-            chunks: 'initial',
-            name: 'vendors',
-            test: 'vendors',
-          },
-          // Merge all the CSS into one file
-          styles: {
-            name: 'styles',
-            test: /\.s?css$/,
-            chunks: 'all',
-            minChunks: 1,
-            reuseExistingChunk: true,
-            enforce: true,
-          },
-        },
-      },
-      // minimize: true,
-      // minimizer: [
-      //   new TerserPlugin({
-      //     extractComments: false,
-      //     terserOptions: {
-      //       ecma: 6,
-      //       compress: { drop_console: true },
-      //       output: { comments: false, beautify: false }
-      //     }
-      //   }),
-      // ],
-    }
+    // optimization: {
+    //   splitChunks: {
+    //     cacheGroups: {
+    //       default: false,
+    //       // Custom common chunk
+    //       bundle: {
+    //         name: 'common',
+    //         chunks: 'all',
+    //         minChunks: 1,
+    //         reuseExistingChunk: true,
+    //         enforce: true,
+    //       },
+    //       // Customer vendor
+    //       vendors: {
+    //         chunks: 'initial',
+    //         name: 'vendors',
+    //         test: 'vendors',
+    //       },
+    //       // Merge all the CSS into one file
+    //       styles: {
+    //         name: 'styles',
+    //         test: /\.s?css$/,
+    //         chunks: 'all',
+    //         minChunks: 1,
+    //         reuseExistingChunk: true,
+    //         enforce: true,
+    //       },
+    //     },
+    //   },
+    //   // minimize: true,
+    //   // minimizer: [
+    //   //   new TerserPlugin({
+    //   //     extractComments: false,
+    //   //     terserOptions: {
+    //   //       ecma: 6,
+    //   //       compress: { drop_console: true },
+    //   //       output: { comments: false, beautify: false }
+    //   //     }
+    //   //   }),
+    //   // ],
+    // }
   },
   css: {
     extract: false
