2a95f92d44fa1945d12b0b9a48159cdf6600bdeb
Nadir <chipchroumme@gmail.com>
2020-10-27 15:11:14 +0400

    fixes + another try to fix products on the inner page

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -5,7 +5,7 @@
     </div>
 
     <!-- INNER PAGE -->
-    <div class="inner-page" :class="{active: currentStreamOpen}" v-body-scroll-lock="isInnerShown" v-if="isInnerShown">
+    <div class="inner-page" :class="{active: currentStreamOpen}" v-if="isInnerShown">
       <div class="inner-page__close" @click="goToHome">
         <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
           <path d="M1 1L17 17M17 1L1 17" stroke="black" stroke-width="1.5" stroke-linecap="round"/>
@@ -21,7 +21,7 @@
         <iframe :src="currentStream.stream.player.url" frameborder="0" v-else></iframe>
         <!-- <Emoji class="stream-page__video_emoji" :stream="currentStream.id" v-if="currentStream.status !== 'NOT_STARTED'" /> -->
       </div>
-      <div class="inner-page__products">
+      <div class="inner-page__products" v-body-scroll-lock="isInnerShown">
         <a target="_blank" :href="product.link" class="inner-page__products-item" v-for="(product,i) in currentStream.products" :key="i">
           <img :src="product.image.small" :alt="decodeURI(product.title)" class="inner-page__products-image">
           <div class="inner-page__products-info">
@@ -29,8 +29,17 @@
             <h3 class="inner-page__products-desc">{{ decodeURI(product.description) }}</h3>
             <div class="inner-page__products-price-wrapper">
               <div class="inner-page__products-prices">
-                <span class="inner-page__products-price-old" v-if="product.isDiscount">{{ product.price.old }}₽</span>
-                <span class="inner-page__products-price-current">{{ product.price.current }}₽</span>
+                <span class="inner-page__products-price-old" v-if="product.isDiscount">{{ product.price.old }}
+                  <svg width="8" height="9" viewBox="0 0 8 9" style="margin-left: -2px" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M2.00869 9.00001V7.36801H0.964687V6.58801H2.00869V5.52001H0.964687V4.60801H2.00869V0.432007H4.26469C5.31269 0.432007 6.08869 0.644007 6.59269 1.06801C7.10469 1.49201 7.36069 2.10801 7.36069 2.91601C7.36069 3.73201 7.08469 4.37201 6.53269 4.83601C5.98069 5.29201 5.16869 5.52001 4.09669 5.52001H3.08869V6.58801H5.06869V7.36801H3.08869V9.00001H2.00869ZM3.94069 4.60801C4.66869 4.60801 5.23269 4.48801 5.63269 4.24801C6.04069 4.00801 6.24469 3.57601 6.24469 2.95201C6.24469 2.40801 6.07669 2.00401 5.74069 1.74001C5.40469 1.47601 4.88069 1.34401 4.16869 1.34401H3.08869V4.60801H3.94069Z" fill="#525252"/>
+                  <path d="M0.844687 5.13601H7.82869V5.73601H0.844687V5.13601Z" fill="#525252"/>
+                  </svg>
+                  </span>
+                <span class="inner-page__products-price-current">{{ product.price.current }}
+                <svg width="10" height="12" viewBox="0 0 10 12" style="margin-left: -2px" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M2.09903 12V9.82399H0.707031V8.78399H2.09903V7.35999H0.707031V6.14399H2.09903V0.575989H5.10703C6.50437 0.575989 7.53903 0.858656 8.21103 1.42399C8.8937 1.98932 9.23503 2.81066 9.23503 3.88799C9.23503 4.97599 8.86703 5.82932 8.13103 6.44799C7.39503 7.05599 6.31237 7.35999 4.88303 7.35999H3.53903V8.78399H6.17903V9.82399H3.53903V12H2.09903ZM4.67503 6.14399C5.6457 6.14399 6.3977 5.98399 6.93103 5.66399C7.47503 5.34399 7.74703 4.76799 7.74703 3.93599C7.74703 3.21066 7.52303 2.67199 7.07503 2.31999C6.62703 1.96799 5.92836 1.79199 4.97903 1.79199H3.53903V6.14399H4.67503Z" fill="black"/>
+                </svg>
+                </span>
               </div>
               <div class="inner-page__products-buy">
                 <span>Купить</span>
