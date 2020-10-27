354149f5871dca1528011f3931267ffc05986f62
Nadir <chipchroumme@gmail.com>
2020-10-27 23:45:14 +0400

    font links changed

--- src/App.vue
+++ src/App.vue
@@ -6,28 +6,28 @@
 
 <style lang="scss">
 @font-face {
-  src: url(/fonts/avenir_demi.otf);
+  src: url("https://widget.live.24ttl.stream/fonts/avenir_demi.otf");
   font-family: Avenir;
   font-weight: 600;
   font-display: swap;
 }
 
 @font-face {
-  src: url(/fonts/avenir_bold.otf);
+  src: url("https://widget.live.24ttl.stream/fonts/avenir_bold.otf");
   font-family: Avenir;
   font-weight: 700;
   font-display: swap;
 }
 
 @font-face {
-  src: url(/fonts/avenir_regularx.ttf);
+  src: url("https://widget.live.24ttl.stream/fonts/avenir_regularx.ttf");
   font-family: Avenir;
   font-weight: 400;
   font-display: swap;
 }
 
 @font-face {
-  src: url(/fonts/avenir_medium.ttf);
+  src: url("https://widget.live.24ttl.stream/fonts/avenir_medium.ttf");
   font-family: Avenir;
   font-weight: 500;
   font-display: swap;
